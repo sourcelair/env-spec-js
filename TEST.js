e0d6e25e3194fb44e61fc8d8322b1942683cf8a2
kanello <akanellop42@gmail.com>
2018-07-17 13:45:36 +0300

    basic validation

--- TEST.js
+++ TEST.js
@@ -1,4 +1,4 @@
-let greeting = "   Hello world!   ";
+/*let greeting = "   Hello world!   ";
 
 console.log(greeting);
 // expected output: "   Hello world!   ";
@@ -11,3 +11,36 @@ let envValues = [];
 let temp = testEnv;
 temp = temp.trim();
 console.log(temp);
+*/
+/*
+console.log('start');
+let regExpA = /ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_/;
+parsedValue = 'DEMssON';
+checkValid = false;
+firstChar = parsedValue.charAt(0);
+for(let j=0; j<=parsedValue.length; j++){
+  if (!(parsedValue.charAt(j).match(regExpA))){
+    console.log(parsedValue.charAt(j))
+    checkValid=true;
+    break;
+  }
+}
+if(checkValid===true){console.log('ERROR:Values cannot contain number or lowercase letters');}
+else if ('0123456789'.indexOf(firstChar) !== -1) {console.log('ERROR:Value cannot start with a number');}
+
+
+
+*/
+
+
+regExp = /ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_/;
+string = "a";
+check = string.match(regExp);
+for (let i =0;i<=string.length;i++){
+
+  temp=string[i];
+  console.log(temp);
+  if(temp.match(regExp)){
+    console.log('wrong');
+  }
+}

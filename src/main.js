0071bdc562d8f57d7d21174cd34393bebf9bcae1
kanello <akanellop42@gmail.com>
2018-07-18 11:19:51 +0300

    More simplifications

--- src/main.js
+++ src/main.js
@@ -1,41 +1,22 @@
-const testEnv = 'DATABASE_URL\nADMIN_EMAIL\nDEBUG\n';
-let tempStringToParse = testEnv; //string for parsing
-let regExpA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_';  //valid chars
-
-console.log(' TURNING \n');
-console.log(testEnv);
-console.log('\n  TO  \n');
+const testEnv = 'DATABASE_URL\nADMIN_EMAIL\nDEBUG';
+const alphanumericThatDoesNotStartWithDigit = /^[A-Z_][0-9A-Z_]*$/;
 
 //PARSING
 let envValues = testEnv.split('\n');//split lines based on \n character
+if(envValues[envValues.length-1]==='\n'){delete(envValues[envValues.length-1])}
 envValues = envValues.filter(element =>{
-  tempStringToParse = element.trim(); //remove whitespace
-  checkValid = false;
-  firstChar = element.charAt(0); //check that every character is valid
-  for(let j=0; j<=element.length; j++){
-    if (regExpA.indexOf(element.charAt(j))<0){
-      checkValid=true;
-      break;
-    }
-  }
-
-  if(checkValid===true){
-    console.log('ERROR:Values cannot contain number or lowercase letters');}
-  else if ('0123456789'.indexOf(firstChar) !== -1) {
-    if(firstChar!=''){console.log('ERROR:Value cannot start with a number');}
-  }
-  else if(element===''){
-    console.log('ERROR:Empty space');
+  if(element.match(alphanumericThatDoesNotStartWithDigit)){
+    return element;
   }
   else{
-    return element;
+    console.log('Error:Wrong value.')
   }
 })
 
 //create HTML output
 outputString = "";
 envValues.forEach(element =>{
-  outputString += `<label for=\"env_spec_${element.toLowerCase()}\">${element}</label>\n`;
-  outputString += `<input id=\"env_spec_${element.toLowerCase()}\" name=\"${element.toLowerCase()}\" />\n`;
+  outputString += `<label for="env_spec_${element.toLowerCase()}">${element}</label>\n`;
+  outputString += `<input id="env_spec_${element.toLowerCase()}" name="${element.toLowerCase()}" />\n`;
 })
 console.log(outputString);

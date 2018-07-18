22f932d02c4890f59a40f254c704e33fdc9f7769
kanello <akanellop42@gmail.com>
2018-07-18 13:51:41 +0300

    second module aattempt

--- src/test1.js
+++ src/test1.js
@@ -1,4 +1,4 @@
-const Main = require('./mainAsModule.js');
+const envSpecToHTML = require('./mainAsModule.js');
 const testEnv = 'DATABASE_URL\nADMIN_EMAIL\nDEBUG\n11';
 console.log('TESTING:\n'+testEnv);
-console.log(Main.envSpecToHTML(testEnv));
+console.log(envSpecToHTML(testEnv));

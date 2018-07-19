6b85b073ae4afea6666aa9292f7d35a4b3b29018
kanello <akanellop42@gmail.com>
2018-07-19 15:05:40 +0300

    installed prettier && file format fixed

--- src/main.test.js
+++ src/main.test.js
@@ -1,25 +1,29 @@
-const envSpecToHTML = require('./main.js');
+const envSpecToHTML = require("./main.js");
 
-test('String starting with number', () => {
-  const testEnv1 = '\n1ADMIN_EMAIL\nDEBUG';
+test("String starting with number", () => {
+  const testEnv1 = "\n1ADMIN_EMAIL\nDEBUG";
   expect(envSpecToHTML(testEnv1)).toEqual(
-  '<label for="env_spec_debug">DEBUG</label>\n'+
-  '<input id="env_spec_debug" name="debug" />\n');
+    '<label for="env_spec_debug">DEBUG</label>\n' +
+      '<input id="env_spec_debug" name="debug" />\n'
+  );
 });
 
-test('String containing wrong characters', () => {
-  const testEnv2 = 'DATABASaaE_URL\nφφADMIN_EMAIL\nDEBUG\n';
+test("String containing wrong characters", () => {
+  const testEnv2 = "DATABASaaE_URL\nφφADMIN_EMAIL\nDEBUG\n";
   expect(envSpecToHTML(testEnv2)).toEqual(
-  '<label for="env_spec_debug">DEBUG</label>\n'+
-  '<input id="env_spec_debug" name="debug" />\n');
+    '<label for="env_spec_debug">DEBUG</label>\n' +
+      '<input id="env_spec_debug" name="debug" />\n'
+  );
 });
 
-test('Valid input', () => {
-  const testEnv3 = 'DATABASE_URL\nADMIN_EMAIL\nDEBUG';
-  expect(envSpecToHTML(testEnv3)).toEqual('<label for="env_spec_database_url">DATABASE_URL</label>\n'+
-  '<input id="env_spec_database_url" name="database_url" />\n'+
-  '<label for="env_spec_admin_email">ADMIN_EMAIL</label>\n'+
-  '<input id="env_spec_admin_email" name="admin_email" />\n'+
-  '<label for="env_spec_debug">DEBUG</label>\n'+
-  '<input id="env_spec_debug" name="debug" />\n');
+test("Valid input", () => {
+  const testEnv3 = "DATABASE_URL\nADMIN_EMAIL\nDEBUG";
+  expect(envSpecToHTML(testEnv3)).toEqual(
+    '<label for="env_spec_database_url">DATABASE_URL</label>\n' +
+      '<input id="env_spec_database_url" name="database_url" />\n' +
+      '<label for="env_spec_admin_email">ADMIN_EMAIL</label>\n' +
+      '<input id="env_spec_admin_email" name="admin_email" />\n' +
+      '<label for="env_spec_debug">DEBUG</label>\n' +
+      '<input id="env_spec_debug" name="debug" />\n'
+  );
 });

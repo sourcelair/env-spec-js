e70fc90b1e92495ac3cdea75c7068be32d0c5259
kanello <akanellop42@gmail.com>
2018-07-19 12:18:25 +0300

    main module exports  envSpecToHTML function, main.test checks all valid values

--- src/main.test.js
+++ src/main.test.js
@@ -7,14 +7,14 @@ test('String starting with number', () => {
   '<input id="env_spec_debug" name="debug" />\n');
 });
 
-test('String containing with not only uppercase latin letters/digits/underscore', () => {
+test('String containing non alaphanumeric characters', () => {
   const testEnv2 = 'DATABASaaE_URL\nφφADMIN_EMAIL\nDEBUG\n';
   expect(envSpecToHTML(testEnv2)).toEqual(
   '<label for="env_spec_debug">DEBUG</label>\n'+
   '<input id="env_spec_debug" name="debug" />\n');
 });
 
-test('All 3 values should pass', () => {
+test('Valid input', () => {
   const testEnv3 = 'DATABASE_URL\nADMIN_EMAIL\nDEBUG';
   expect(envSpecToHTML(testEnv3)).toEqual('<label for="env_spec_database_url">DATABASE_URL</label>\n'+
   '<input id="env_spec_database_url" name="database_url" />\n'+

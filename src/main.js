76676c3b5483b5d1f6ba58ae4a7a9f1c91bd9f02
kanello <akanellop42@gmail.com>
2018-07-23 15:23:01 +0300

    - changed parseVarFromType function using includes and split methods - changed comments - changed tests - - envSpecToHTML method can parse and validate enviromental variables anf their types

--- src/main.js
+++ src/main.js
@@ -1,51 +1,65 @@
 //function that checks if the env value is valid
 //value cannot start with a digit and must only contain alphanumeric characters or an underscore
 const checkValidationOfValues = envSpecString => {
-  const validValues = ['color','date','datetime-local','email','month','number','password','tel','text','time','url','week','default'];
+  const validTypes = ['color','date','datetime-local','email','month','number','password','tel','text','time','url','week'];
   const alphanumericThatDoesNotStartWithDigit = /^[A-Z_][0-9A-Z_]*$/;
-  let envValArray = parseVarFromVal(envSpecString.split("\n")); //split lines based on \n character and parse them
+  let envValArray = parseVarFromType(envSpecString.trim().split("\n")); //split lines based on \n character and parse them
+  let checkValidation = true;
 
-  return envValArray = envValArray.filter( element =>{ //check for valid variables AND values
-    if(element[0].match(alphanumericThatDoesNotStartWithDigit)&&(validValues.includes(element[1]))){
+  //envValArray is a two-dimensional array containing only the types and variables
+  //element[0] is the variable e.g. ADMIN_EMAIL
+  //element[1] is the type e.g. test
+  envValArray = envValArray.filter( element =>{ //check for valid variables AND types
+    if(element[0].match(alphanumericThatDoesNotStartWithDigit)&&(validTypes.includes(element[1]))){
       return element;
     }
+    else{
+      checkValidation = false;
+    }
   })
+
+  //in case of one or more invalid variables or types, return error
+  if(checkValidation === true){return envValArray;}
+  else{return null;}
 };
 
-//function that separates the variables from their values and returns them as separated arrays
-const parseVarFromVal = envSpecAsArray => {
-  let envArrayParsed = [];
-  let parsPos = -1;
-  return envArrayParsed = envSpecAsArray.map( element =>{
-    parsPos = element.indexOf(":");
-    if(parsPos<0){
-      parsPos=element.length
-      return [element.substring(0,parsPos).trim(),'default'];
+
+//function that separates the variables from their types and returns them as a two-dimensional array
+const parseVarFromType = envSpecAsArray => {
+  return envSpecAsArray = envSpecAsArray.map( element =>{
+    if (element.includes(":")){
+      element = element.split(":");
+      return [element[0].trim(),element[1].trim()];
     }
-    else{
-      return [element.substring(0,parsPos).trim(),element.substring(parsPos+1,).trim()];
+    else{ //if untyped enviromental variable set default as text
+      return [element.trim(),'text'];
     }
   })
 };
 
 
 //function that returns the HTML code as a string
+//envValues is a two-dimensional array containing the variable name and its type
 const outputHTML = envValues => {
   //create HTML format
-  envValues = envValues.map(
-    element =>
-      `<label for="env_spec_${element[0].toLowerCase()}">${element[0]}</label>\n` +
-      `<input id="env_spec_${element[0].toLowerCase()}" name="${element[0].toLowerCase()}" />\n`
-  );
-  //return as string value
-  return envValues.join("");
+  if (envValues){
+    envValues = envValues.map(
+      element =>
+        `<label for="env_spec_${element[0].toLowerCase()}">${element[0]}</label>\n` +
+        `<input id="env_spec_${element[0].toLowerCase()}" name="${element[0].toLowerCase()}" type="${element[1]}" />\n`
+
+    );
+    //return as string value
+    return envValues.join("");
+  }
+  //in case of syntax error , print HTML format
+  return "Error:Wrong Syntax";
 };
 
 
-//function that calls functions for final output
+//function for final output
 const envSpecToHTML = envSpec => {
   return outputHTML(checkValidationOfValues(envSpec));
 };
 
-console.log(envSpecToHTML("DATABASE_URL: notgood\nADMIN_EMAIL\nDEBUG :   text"));
 module.exports = envSpecToHTML;

803e995da9cabdf177df5eea56821a1b794eec65
kanello <akanellop42@gmail.com>
2018-07-31 13:00:42 +0300

    Started Restricted choices feature In fuction checkValidationOfValues, we check if there is syntax for restricted choices and pass an extra argument in element so outputHTML function can print the right html string according to what follows the environmental variable(type or restricted value) Added tests

--- src/main.test.js
+++ src/main.test.js
@@ -1,12 +1,25 @@
 const envSpecToHTML = require("./main.js");
 
-test("Valid input", () => {
+test("Valid input with type", () => {
   const testEnv = "DATABASE_URL\nADMIN_EMAIL:email";
   expect(envSpecToHTML(testEnv)).toEqual(
     '<label for="env_spec_database_url">DATABASE_URL</label>\n' +
       '<input id="env_spec_database_url" name="database_url" type="text" />\n' +
       '<label for="env_spec_admin_email">ADMIN_EMAIL</label>\n' +
       '<input id="env_spec_admin_email" name="admin_email" type="email" />\n'
+  );
+});
+
+test("Valid input with restricted choices", () => {
+  const testEnv = "DATABASE_URL\nNUMBER :[0,1]";
+  expect(envSpecToHTML(testEnv)).toEqual(
+    '<label for="env_spec_database_url">DATABASE_URL</label>\n' +
+      '<input id="env_spec_database_url" name="database_url" type="text" />\n' +
+      `<label for="env_spec_admin_number">NUMBER</label>\n` +
+      `<select id="env_spec_admin_number" name="admin_number">\n` +
+      `  <option value="0">0</option>\n` +
+      `  <option value="1">1</option>\n` +
+      `</select>\n`
   );
 });
 
@@ -43,4 +56,9 @@ test("Untyped environmental variable", () => {
 test("Multiple invalid variables and types", () => {
   const testEnv3 = "DATABASE_URLαα: αα\nADMIN_EMAIL:\n1DEBUG";
   expect(envSpecToHTML(testEnv3)).toEqual("Error:Wrong Syntax");
+});
+
+test("Wronf Syntax for restricted choices", () => {
+  const testEnv = "DATABASE_URL\nADMIN_EMAIL:email\nDATA:[0,";
+  expect(envSpecToHTML(testEnv)).toEqual("Error:Wrong Syntax");
 });

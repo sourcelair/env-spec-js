d9f5d4d4ad561edf28682798dd3ffdbe1ef752ab
kanello <akanellop42@gmail.com>
2018-08-20 14:19:07 +0300

    changed format

--- demo/main.js
+++ demo/main.js
@@ -3,12 +3,12 @@ button.addEventListener("click", function getHTML() {
   envSpec
     .parse(inputTextArea.value)
     .then(entries => entries.html())
-    .then(html => outputTextArea.value = html)
+    .then(html => (outputTextArea.value = html))
     .catch(error => error);
 
-    envSpec
-      .parse(inputTextArea.value)
-      .then(entries => entries.html())
-      .then(html => document.getElementById("finalOutput").innerHTML = html)
-      .catch(error =>   document.getElementById("finalOutput").innerHTML = error)
+  envSpec
+    .parse(inputTextArea.value)
+    .then(entries => entries.html())
+    .then(html => (document.getElementById("finalOutput").innerHTML = html))
+    .catch(error => (document.getElementById("finalOutput").innerHTML = error));
 });
